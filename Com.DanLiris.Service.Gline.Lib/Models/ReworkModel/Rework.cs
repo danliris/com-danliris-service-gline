6287f88c5efdc6e932d8a7bbb6b68e8fda61dc5a
MatthewADIAN <hartonomatthew0@gmail.com>
2023-05-11 10:28:50 +0700

    Delete Shift and Modify SummaryOperator

---
+++ Com.DanLiris.Service.Gline.Lib/Models/ReworkModel/Rework.cs
@@ -0,0 +1,23 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Com.DanLiris.Service.Gline.Lib.Models.ReworkModel
+{
+    public class Rework : StandardEntity<Guid>
+    {
+        [Required]
+        [MaxLength(32)]
+        public string npk { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string nama_operator { get; set; }
+        public int qty_rework { get; set; }
+        public Guid id_ro { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string rono { get; set; }
+        public Guid id_line { get; set; }
+        public int nama_line { get; set; }
+    }
+}
---
+++ Com.DanLiris.Service.Gline.Lib/Models/TransaksiModel/SummaryQc.cs
@@ -0,0 +1,25 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Com.DanLiris.Service.Gline.Lib.Models.TransaksiModel
+{
+    public class SummaryQc : StandardEntity<Guid>
+    {
+        [Required]
+        [MaxLength(32)]
+        public string npk { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string nama { get; set; }
+        public int total_pass { get; set; }
+        public int total_reject { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string rono { get; set; }
+        public TimeSpan setting_time { get; set; }
+        public DateTime setting_date { get; set; }
+    }
+}
