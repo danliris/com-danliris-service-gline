a640a47d488c740abe91c58099224f1090932046
YOKANA <yokana.adita@outlook.com>
2023-05-23 13:24:31 +0700

    Update Setting Ro API

--- Com.DanLiris.Service.Gline.Lib/ViewModels/SettingRoViewModel.cs
+++ Com.DanLiris.Service.Gline.Lib/ViewModels/SettingRoViewModel.cs
@@ -3,6 +3,7 @@ using Com.DanLiris.Service.Gline.Lib.ViewModels.MasterViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace Com.DanLiris.Service.Gline.Lib.ViewModels
@@ -21,12 +22,15 @@ namespace Com.DanLiris.Service.Gline.Lib.ViewModels
         public string kode_unit { get; set; }
         public string nama_unit { get; set; }
         public int quantity { get; set; }
+        public bool isEdit { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
+            GlineDbContext dbContext = validationContext == null ? null : (GlineDbContext)validationContext.GetService(typeof(GlineDbContext));
+
             if (string.IsNullOrWhiteSpace(rono))
             {
-                yield return new ValidationResult("RONo is required", new List<string> { "nama_line" });
+                yield return new ValidationResult("RONo is required", new List<string> { "rono" });
             }
 
             if (jam_target <= 0)
@@ -47,7 +51,18 @@ namespace Com.DanLiris.Service.Gline.Lib.ViewModels
             if (setting_date.Date < DateTime.Now.Date)
             {
                 yield return new ValidationResult($"Setting Date must be equal or greater than {DateTime.Now.Date}", new List<string> { "setting_date" });
-            } 
+            }
+
+            var duplicateRONo = dbContext.SettingRo.Where(x => x.rono == rono && x.id_line == id_line).Count();
+            if (duplicateRONo > 0 && !isEdit)
+            {
+                yield return new ValidationResult("RONo is already exist", new List<string> { "rono" });
+            }
+
+            if(quantity == 0)
+            {
+                yield return new ValidationResult("Quantity must be greater than 0", new List<string> { "quantity" });
+            }
 
         }
     }
